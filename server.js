18448ad6e967b99a6a472c441a557eaa2e3eb466
mertpinarbasi <mpinarbasi35@gmail.com>
2022-01-28 22:15:16 +0300

    project initialization

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+
+// Route files
+const bootcamps = require("./routes/bootcamp");
+
 // Load env variables
 dotenv.config({
   path: "./config/config.env",
 });
 // Server initialization
 const app = express();
-
+app.use("/api/v1/bootcamps", bootcamps);
 // Port configuration
 const PORT = process.env.PORT || 5000;
 
 // Connect to the port
-app.listen(PORT, `Server running in mode on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running in mode on port ${PORT}`);
+});
